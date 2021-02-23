d3a32873d56ca6ab9c75012684098e1ee037f196
CristoferGarcia1984 <60959942+CristoferGarcia1984@users.noreply.github.com>
2021-02-23 12:27:35 +0100

    Add catch statement to deal w/ HTTP request errors

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,34 @@ import SearchBar from './SearchBar';
 import ImageList from './ImageList';
 
 class App extends React.Component {
-  state = { images: [] };
+  state = { images: [], error: '' };
 
   onSearchSubmit = async (term) => {
-    const response = await unsplash.get('/search/photos', {
-      params: { query: term },
-    });
-
-    this.setState({ images: response.data.results });
+    await unsplash
+      .get('/search/photos', {
+        params: { query: term },
+      })
+      .then((res) => {
+        console.log(res);
+        this.setState({ images: res.data.results });
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        this.setState({ error: errorMessage });
+      });
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmitProp={this.onSearchSubmit} />
+        {this.state.error ? (
+          <div style={{ marginBottom: '10px' }}>{this.state.error}</div>
+        ) : (
+          <div style={{ marginBottom: '10px' }}>
+            {this.state.images.length} images found
+          </div>
+        )}
         <ImageList images={this.state.images} />
       </div>
     );
